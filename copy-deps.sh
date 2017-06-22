1833f5392063bab81c4f9e5d0c1d37650f60c152
Vladimir DOMBROVSKI <vladimir@openio.io>
2017-06-22 10:04:26 +0200

    Full redesign using UIKIT, add paginator

--- copy-deps.sh
+++ copy-deps.sh
@@ -3,5 +3,10 @@ cp node_modules/angular-file-upload/dist/angular-file-upload.min.js static/vendo
 cp node_modules/bootstrap/dist/css/bootstrap.min.css static/vendor/
 cp node_modules/lightbox2/dist/js/lightbox.min.js static/vendor/
 cp node_modules/lightbox2/dist/css/lightbox.min.css static/vendor/
+
+cp node_modules/uikit/dist/css/uikit.min.css static/vendor/
+cp node_modules/uikit/dist/js/uikit.min.js static/vendor/
+cp node_modules/uikit/dist/js/uikit-icons.min.js static/vendor/
+
 cp -r node_modules/bootstrap/fonts/ static/
 cp -r node_modules/lightbox2/dist/images/ static/
